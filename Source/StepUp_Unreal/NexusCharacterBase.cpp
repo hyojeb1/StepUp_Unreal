18d71dc930ca30069f6dc4bd5d2c9a66adfaee45
K2312120025\Hyoje <gywpjang@naver.com>
2026-04-02 19:28:22 +0900

    [GAS 1강 완] 캐릭터
    - 스프링 암 사용법 익힘

--- Source/StepUp_Unreal/NexusCharacterBase.cpp
+++ Source/StepUp_Unreal/NexusCharacterBase.cpp
@@ -1,16 +1,59 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+/*****************************************************************//**
+ * \file   NexusCharacterBase.cpp
+ * \brief  1강 : https://www.youtube.com/watch?v=1QgGyndH_u4
+ * 
+ * \author Hyoje
+ * \date   2026-4-2 
+ *********************************************************************/
 
 #include "NexusCharacterBase.h"
 #include "AbilitySystemComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
-// Sets default values
+
+/**
+ * 생성자에서 하는 일은 두 덩어리입니다.
+ * 
+ * A. ASC 생성과 복제 설정
+ *	CreateDefaultSubobject
+ *	SetIsReplicated(true)
+ *	SetReplicationMode(...)
+ * B. 캐릭터 기본 세팅
+ *	캡슐 크기
+ *	컨트롤러 회전 비활성화
+ *	이동 방향 회전
+ *	매쉬 정상화
+ *  캠
+ *	붐
+ * 
+ * 여기서 중요한 건 GAS 핵심은 A 덩어리라는 점입니다.
+ * B는 부가 설정입니다.
+ * 
+ */
 ANexusCharacterBase::ANexusCharacterBase()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	//---A---
 	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true); // 네트워크 동기화됨
+	AbilitySystemComponent->SetReplicationMode(ASCReplicationMode);
+
+	//---B---
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.f);
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+	UCharacterMovementComponent* CharacterMovementComponent = GetCharacterMovement();
+	CharacterMovementComponent->bOrientRotationToMovement = true;
+	//CharacterMovementComponent->bUseControllerDesiredRotation = false;
+	CharacterMovementComponent->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
+	GetMesh()->SetRelativeLocationAndRotation(
+		FVector(0, 0, -GetCapsuleComponent()->GetScaledCapsuleHalfHeight()),
+		FRotator(0, -90.0f, 0)
+	);
 }
 
 // Called when the game starts or when spawned
@@ -34,18 +77,40 @@ void ANexusCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInput
 
 }
 
+// ASC getter
 UAbilitySystemComponent* ANexusCharacterBase::GetAbilitySystemComponent() const
 {
 	return AbilitySystemComponent;
 }
 
+/**
+ * 서버에서 캐릭터가 possession되는 시점에 ASC ActorInfo를 초기화한다.
+ * 여기서는 Owner = this, Avatar = this다.
+ * 즉, 이 Character가 ASC의 논리적 소유자이면서,
+ * Ability가 실제로 적용되고 애니메이션/몽타주 등이 실행되는 현재 Avatar이기도 하다.
+ * 
+ * \param NewController
+ */
 void ANexusCharacterBase::PossessedBy(AController* NewController)
 {
 	Super::PossessedBy(NewController);
+
+	if (AbilitySystemComponent)
+	{
+		AbilitySystemComponent->InitAbilityActorInfo(this, this);
+	}
 }
 
+/**
+ * 클라에서도 서버와 마찬가지로 진행한다.
+ */
 void ANexusCharacterBase::OnRep_PlayerState()
 {
 	Super::OnRep_PlayerState();
+
+	if (AbilitySystemComponent)
+	{
+		AbilitySystemComponent->InitAbilityActorInfo(this, this);
+	}
 }
 
--- Source/StepUp_Unreal/NexusCharacterBase.h
+++ Source/StepUp_Unreal/NexusCharacterBase.h
@@ -1,6 +1,6 @@
 /*****************************************************************//**
  * \file   NexusCharacterBase.h
- * \brief  결국 이건 그거임. 베이스!!! 넥서스 게임의 적과 플레이어가 상속받을...
+ * \brief  1강 : https://www.youtube.com/watch?v=1QgGyndH_u4
  * 
  * \author Hyoje
  * \date   2026-4-2 
@@ -11,12 +11,17 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "AbilitySystemInterface.h"
+#include "AbilitySystemComponent.h"
 #include "GameplayEffectTypes.h"
 #include "NexusCharacterBase.generated.h"
 
+/**
+ * IAbilitySystemInterface가 없으면 GAS가 이 액터의 ASC를 표준적으로 찾기 어렵다..
+ */
 UCLASS()
 class STEPUP_UNREAL_API ANexusCharacterBase : public ACharacter, public IAbilitySystemInterface
 {
+
 	GENERATED_BODY()
 
 public:
@@ -32,12 +37,22 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	// 갖고 오기
+	/**
+	 * GetAbilitySystemComponent()는 ASC 접근 창구다.
+	 */
 	virtual class UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
-	// 서버
+	/**
+	 * PossessedBy()는 서버 측 초기화 지점이다.
+	 * 
+	 * \param NewController
+	 */
 	virtual void PossessedBy(AController* NewController) override;
-	// 클라
+
+	/**
+	 * OnRep_PlayerState()는 클라이언트 측 초기화 지점이다.
+	 * 
+	 */
 	virtual void OnRep_PlayerState() override;
 
 protected:
@@ -45,8 +60,11 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Abilities")
 	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
 
-	// 타입, 
+	/**
+	 * 왜 ASCReplicationMode를 변수로 두는가
+	 * → 플레이어와 AI에서 복제 정책을 다르게 쓸 수 있게 하려는 것
+	 */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Abilities")
-	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent2;
+	EGameplayEffectReplicationMode ASCReplicationMode = EGameplayEffectReplicationMode::Mixed;
 
 };
